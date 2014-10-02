7db4a0a0e63b4b25d784775a00ecf52a654aaab9
Anthonyeef <anthonyeef@gmail.com>
2014-10-02 16:52:07 +0200

    add another sentence using the firefox browser

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 cout<<"hello, world!"<<endl;
-cout<<"and just another change to this code."<<endl
+cout<<"and just another change to this code."<<endl;
+cout<<"using another browser to log in github and do this change."<<endl;
 return 0;
 }
